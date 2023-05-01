14763792f398017a400fcb72dbbcd889adcf9e0c
VolodymyrBakun <volodymyr.bakun@gmail.com>
2023-05-01 20:30:20 +0300

    task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const markup = createGalleryMarkup(galleryItems)
+
+galleryEl.insertAdjacentHTML('afterbegin', markup)
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => { return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+}).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+  })
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import  throttle  from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TIME_KEY = "videoplayer-current-time";
+
+iframe.addEventListener('load' , setTime);
+
+function setTime(params) {
+    if (localStorage.getItem(TIME_KEY) === null) {
+        return
+    } else {
+        player.setCurrentTime(localStorage.getItem(TIME_KEY))
+    }
+}
+
+const saveTime = function (data) {
+     const currentTime = JSON.stringify(data.seconds);
+      localStorage.setItem(TIME_KEY, currentTime)
+
+    }
+
+
+player.on('timeupdate', throttle (saveTime, 1000));
 
