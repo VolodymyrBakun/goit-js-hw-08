40e83fb2ae3426d43261bb1bb7c879db43757bd6
VolodymyrBakun <volodymyr.bakun@gmail.com>
2023-05-01 22:44:01 +0300

    Update 03-feedback.js

---
+++ src/js/03-feedback.js
@@ -0,0 +1,53 @@
+import  throttle  from "lodash.throttle";
+
+const STORAGE_KEY = "feedback-form-state";
+const inputEmailEL = document.querySelector('[name="email"]');
+const messageEl = document.querySelector('[name="message"]');
+const buttonEl = document.querySelector('button');
+const formEl = document.querySelector('.feedback-form');
+const data = {
+    email: "",
+    message: ""
+}
+
+
+if (localStorage.getItem(STORAGE_KEY) !== null) {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    const parsedData = JSON.parse(savedData)
+inputEmailEL.value = parsedData.email;
+messageEl.textContent = parsedData.message;
+}
+
+
+
+
+formEl.addEventListener("input", throttle(handleInput, 500));
+
+
+function handleInput(event) {  
+    
+if (event.target.tagName === "INPUT") {
+    data.email = event.target.value;
+} if (event.target.tagName === "TEXTAREA") {
+    data.message = event.target.value;
+} 
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
+}
+
+buttonEl.addEventListener("click", event => {
+    if (localStorage.getItem(STORAGE_KEY) !== null) {
+        const savedData = localStorage.getItem(STORAGE_KEY);
+        const parsedData = JSON.parse(savedData)
+    inputEmailEL.value = parsedData.email;
+    messageEl.textContent = parsedData.message;
+    
+        console.log(parsedData);
+    }
+    event.preventDefault();
+    formEl.reset();
+    messageEl.textContent = '';
+    localStorage.clear();
+})
+
+
+
