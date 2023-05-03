9e38e52abc01718497328fc61d0641e7ff552e0f
VolodymyrBakun <volodymyr.bakun@gmail.com>
2023-05-03 19:11:09 +0300

    fixed issues

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,53 +1,53 @@
-import  throttle  from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = "feedback-form-state";
+const STORAGE_KEY = 'feedback-form-state';
 const inputEmailEL = document.querySelector('[name="email"]');
 const messageEl = document.querySelector('[name="message"]');
 const buttonEl = document.querySelector('button');
 const formEl = document.querySelector('.feedback-form');
-const data = {
-    email: "",
-    message: ""
-}
 
+const data = {
+  email: '',
+  message: '',
+};
 
 if (localStorage.getItem(STORAGE_KEY) !== null) {
-    const savedData = localStorage.getItem(STORAGE_KEY);
-    const parsedData = JSON.parse(savedData)
-inputEmailEL.value = parsedData.email;
-messageEl.textContent = parsedData.message;
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  const parsedData = JSON.parse(savedData);
+  inputEmailEL.value = parsedData.email;
+  messageEl.textContent = parsedData.message;
+
+  data.email = `${parsedData.email}`;
+  data.message = `${parsedData.message}`;
 }
 
+formEl.addEventListener('input', throttle(handleInput, 500));
 
-
-
-formEl.addEventListener("input", throttle(handleInput, 500));
-
-
-function handleInput(event) {  
-    
-if (event.target.tagName === "INPUT") {
+function handleInput(event) {
+  if (event.target.tagName === 'INPUT') {
     data.email = event.target.value;
-} if (event.target.tagName === "TEXTAREA") {
+  }
+  if (event.target.tagName === 'TEXTAREA') {
     data.message = event.target.value;
-} 
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
+  }
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 }
 
-buttonEl.addEventListener("click", event => {
-    if (localStorage.getItem(STORAGE_KEY) !== null) {
-        const savedData = localStorage.getItem(STORAGE_KEY);
-        const parsedData = JSON.parse(savedData)
-    inputEmailEL.value = parsedData.email;
-    messageEl.textContent = parsedData.message;
-    
-        console.log(parsedData);
-    }
-    event.preventDefault();
-    formEl.reset();
-    messageEl.textContent = '';
-    localStorage.clear();
-})
+buttonEl.addEventListener('click', event => {
+  result = {};
 
+  if (data.email) {
+    result.email = data.email;
+  }
+  if (data.message) {
+    result.message = data.message;
+  }
+  console.log(result);
 
-
+  event.preventDefault();
+  formEl.reset();
+  messageEl.textContent = '';
+  localStorage.clear();
+  data.email = '';
+  data.message = '';
+});
